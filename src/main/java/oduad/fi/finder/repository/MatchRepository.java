5c74555edb635dca2b6c702395275c1d8d3446e7
Oduad <aragon_1123@hotmail.com>
2025-03-02 22:30:42 -0600

    Match logic started

--- src/main/java/oduad/fi/finder/repository/MatchRepository.java
+++ src/main/java/oduad/fi/finder/repository/MatchRepository.java
@@ -4,9 +4,10 @@ import oduad.fi.finder.entity.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
-
-
+    List<Match> findByUser1IdOrUser2Id(Long user1Id, Long user2Id);
 
 }
--- src/main/java/oduad/fi/finder/service/MatchServiceImp.java
+++ src/main/java/oduad/fi/finder/service/MatchServiceImp.java
@@ -1,10 +1,20 @@
 package oduad.fi.finder.service;
 
 import oduad.fi.finder.entity.Match;
+import oduad.fi.finder.repository.MatchRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MatchServiceImp implements MatchService{
+
+    MatchRepository matchRepository;
+
+    public MatchServiceImp(MatchRepository matchRepository){
+        this.matchRepository = matchRepository;
+    }
 
     @Override
     public Match createMatch(Long userId1, Long userId2) {
@@ -13,7 +23,7 @@ public class MatchServiceImp implements MatchService{
 
     @Override
     public List<Match> getMatchesByUser(Long userId) {
-        return null;
+        return matchRepository.findByUser1IdOrUser2Id(userId, userId);
     }
 
     @Override
