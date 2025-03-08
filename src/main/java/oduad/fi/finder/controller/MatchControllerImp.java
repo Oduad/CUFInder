b2b676d012397afff237e21cd3b1b21218e02cd5
Oduad <aragon_1123@hotmail.com>
2025-03-07 23:52:21 -0600

    MatchControllerImp modified

--- src/main/java/oduad/fi/finder/controller/MatchControllerImp.java
+++ src/main/java/oduad/fi/finder/controller/MatchControllerImp.java
@@ -1,12 +1,27 @@
 package oduad.fi.finder.controller;
 
 import oduad.fi.finder.entity.Match;
+import oduad.fi.finder.repository.MatchRepository;
+import oduad.fi.finder.repository.UserRepository;
+import oduad.fi.finder.service.MatchServiceImp;
+import oduad.fi.finder.service.UserServiceImp;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/fisfinder")
 public class MatchControllerImp implements MatchController{
 
-    
+    private final MatchServiceImp matchServiceImp;
+    //@Autowired
+    private final MatchRepository matchRepository;
+
+    public MatchControllerImp(MatchServiceImp matchServiceImp, MatchRepository matchRepository){
+        this.matchRepository = matchRepository;
+        this.matchServiceImp = matchServiceImp;
+    };
 
     @Override
     public Match createMatch(Long userId1, Long userId2) {
@@ -15,7 +30,7 @@ public class MatchControllerImp implements MatchController{
 
     @Override
     public List<Match> getMatchesByUser(Long userId) {
-        return null;
+        return matchServiceImp.getMatchesByUser(userId);
     }
 
     @Override
